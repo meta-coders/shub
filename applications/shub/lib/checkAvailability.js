3fc98b05d24bba1582ff3c98072eacc06dcc08ae
root <root@shub.c.ws-chat-190007.internal>
2018-02-08 19:53:54 +0000

    Improved API methods

--- applications/shub/lib/checkAvailability.js
+++ applications/shub/lib/checkAvailability.js
@@ -6,5 +6,21 @@ api.checkAvailability = function(sessionId, callback) {
     from: true
   };
 
-  this.db.mysql.query(options, callback);
+  this.db.mysql.query(options, (err, result) => {
+    if (!result[0]) {
+      callback(err);
+      return;
+    }
+
+    const uid = result[0].user_id;
+
+    const options = {
+      query: '*',
+      table: 'students',
+      search: `(select profile_id from users where id=${uid})`,
+      from: true
+    };
+
+    this.db.mysql.query(options, (err, result) => callback(err, result[0].class_id));
+  });
 };
